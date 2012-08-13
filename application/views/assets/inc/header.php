4298f07bf0702985910b663849fdb6ee4ddea78c
Mario Cuba <mario@mariocuba.net>
2012-08-13 15:48:52 -0430

    Linked styles and js to header. Created footer.

---
+++ application/views/assets/inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>SAT &mdash; Sistema Automatizado de Tickets</title>
+
+	<link rel="stylesheet" href="<?php echo $this->resource->css('bootstrap.css'); ?>" />
+	<link rel="stylesheet" href="<?php echo $this->resource->css('bootstrap-responsive.css'); ?>" />
+	<link rel="stylesheet" href="<?php echo $this->resource->css('app.css'); ?>" />
+
+	<script src="<?php echo $this->resource->js('jquery.js'); ?>"></script>
+	<script src="<?php echo $this->resource->js('bootstrap.js'); ?>" /></script>
+	<script src="<?php echo $this->resource->js('app.js'); ?>" /></script>
+</head>
+
+<body>

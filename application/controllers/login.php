f358adead0af2df211ac639b66dea3f12e1e979c
Mario Cuba <mario@mariocuba.net>
2012-08-13 15:49:03 -0430

    Example login controller.

---
+++ application/controllers/login.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* Handles the login and first ticket.
+*
+* @package		SAT
+* @subpackage	Controllers
+*/
+class Login extends SAT_Controller {
+
+	/**
+	* Displays the main site and the login form.
+	*
+	* @access	public
+	*/
+	public function index() {
+
+	}
+}
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */

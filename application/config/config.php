bd487f5708d3c6bc9368c15c06c600f01de81340
Mario Cuba <mario@mariocuba.net>
2012-08-13 11:44:17 -0430

    Added logging and removed base index.

--- application/config/config.php
+++ application/config/config.php
@@ -51,7 +51,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -211,7 +211,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = array(1,3);
 
 /*
 |--------------------------------------------------------------------------

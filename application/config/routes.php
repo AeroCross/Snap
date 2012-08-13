c5f3492dd35f904015b18904f701b328ece39f7b
Mario Cuba <mario@mariocuba.net>
2012-08-13 15:16:27 -0430

    Changed default route.

--- application/config/routes.php
+++ application/config/routes.php
@@ -64,8 +64,8 @@
 |
 */
 
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
+$route['default_controller'] = 'login';
+$route['404_override'] = 'login';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */

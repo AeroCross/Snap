8c88bb1969e24b9ff5a714218dd17ad0397ac0cf
Mario Cuba <mario@mariocuba.net>
2012-08-22 14:21:41 -0430

    Autoload the Icon Helper.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,7 @@ $autoload['libraries'] = array('common/resource', 'debug/kint');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'html', 'form');
+$autoload['helper'] = array('url', 'html', 'form', 'icon');
 
 
 /*

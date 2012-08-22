1a23d568df9458933c836305c0cf8324db9c234e
Mario Cuba <mario@mariocuba.net>
2012-08-22 14:21:01 -0430

    Commented header. Added new fonts.

--- application/views/assets/inc/header.php
+++ application/views/assets/inc/header.php
@@ -4,13 +4,18 @@
 <head>
 
 	<meta charset="utf-8" />
-	<title>SAV &mdash; Sistema de Atención Virtual</title>
+	<title>SAV &mdash; Sistema de Asistencia Virtual</title>
 
+	<!-- fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700|Telex' rel='stylesheet' type='text/css'>
+	
+	<!-- styles -->
 	<link rel="stylesheet" href="<?php echo $this->resource->css('bootstrap.css'); ?>" />
 	<link rel="stylesheet" href="<?php echo $this->resource->css('bootstrap-responsive.css'); ?>" />
 	<link rel="stylesheet" href="<?php echo $this->resource->css('font-awesome.css'); ?>" />
 	<link rel="stylesheet" href="<?php echo $this->resource->css('app.css'); ?>" />
 
+	<!-- scripts -->
 	<script src="<?php echo $this->resource->js('jquery.js'); ?>"></script>
 	<script src="<?php echo $this->resource->js('bootstrap.js'); ?>" /></script>
 	<script src="<?php echo $this->resource->js('app.js'); ?>" /></script>

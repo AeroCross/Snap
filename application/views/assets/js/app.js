fc41385afd2d84179a40fa062c9e8230ff463d24
Mario Cuba <mario@mariocuba.net>
2012-08-22 22:50:06 -0430

    Initialize alerts.

--- application/views/assets/js/app.js
+++ application/views/assets/js/app.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function() {
 
+	// initialize alerts
+	jQUery('.alert').alert();
+	
 	// submit login form when clicked
 	jQuery('#login-submit').on('click', function() {
 		jQuery(this).parent('form').submit();

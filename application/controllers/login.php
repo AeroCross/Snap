d2943fad398c7a941d50084fc9bb5542b6145fee
Mario Cuba <mario@mariocuba.net>
2012-08-23 10:36:09 -0430

    Basic authentication.

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -14,8 +14,53 @@ class Login extends SAV_Controller {
 		// load the notification presenter
 		$this->load->presenter('notification');
 	}
+	
 	public function index() {
 		$this->layout = 'login';
+
+		if ($this->input->post() != FALSE) {
+			$this->presenter->notification->create($this->_login());
+		}
+	}
+
+	public function _login() {
+		$this->load->library('form_validation');
+
+		// all fields required
+		$this->form_validation->set_rules('username', 'Nombre de Usuario', 'required');
+		$this->form_validation->set_rules('password', 'Contraseña', 'required');
+
+		if (!$this->form_validation->run()) {
+			return array(
+				'status'	=> 'required',
+				'title'		=> 'Todos los campos son requeridos.',
+				'message'	=> 'Intente nuevamente.',
+				'type'		=> 'warning'
+			);
+		}
+
+		// try to log in the user
+		$username	= $this->input->post('username');
+		$password	= $this->input->post('password');
+
+		$this->load->model('sav_user');
+
+		if ($this->sav_user->login($username, $password) === FALSE) {
+			return array(
+				'status'	=> 'login_failed',
+				'title'		=> 'Usuario o contraseña incorrecto.',
+				'message'	=> 'Intente nuevamente.',
+				'type'		=> 'warning'
+			);
+
+		// logged in
+		} else {
+			return array(
+				'status'	=> 'logged_in',
+				'message'	=> 'Sesión iniciada.',
+				'type'		=> 'success'
+			);
+		}
 	}
 }
 

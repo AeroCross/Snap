1ba9060365bb4c0a6d0c407b622d6ff44a6b298a
Mario Cuba <mario@mariocuba.net>
2012-08-24 11:35:01 -0430

    Renamed the default layout to site, then the default became app.

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -12,27 +12,24 @@
 
 			<ul class="nav">
 
-				<li><?php echo anchor('#', 'Guía de Usuario'); ?></li>
-				<li><?php echo anchor('#', 'Preguntas Frecuentes'); ?></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Nueva <b class="caret"></b></a>
+
+					<ul class="dropdown-menu">
+
+						<li><?php echo anchor('#', 'Consulta'); ?></li>
+						<li><?php echo anchor('#', 'Solicitud de Servicio'); ?></li>
+
+					</ul>
+
+				</li>
+
+				<li><?php echo anchor('#', 'Consultas'); ?></li>
 				<li><?php echo anchor('#', 'Soporte'); ?></li>
 
 			</ul>
 
-			<?php if ($this->init->hasSession()): ?>
-
-				<p class="navbar-text pull-right">Sesión iniciada como <strong><?php echo $this->session->userdata('name'); ?></strong> &mdash; <?php echo anchor('dashboard', 'ir a area de cliente'); ?> | <?php echo anchor('logout', 'salir'); ?></p>
-
-			<?php else: ?>
-
-				<?php echo form_open('login', array('class' => 'navbar-form pull-right')); ?>
-
-					<input type="text" class="span2" name="username" placeholder="Usuario" />
-					<input type="password" class="span2" name="password" placeholder="Contraseña" />
-					<button class="btn" id="login-submit"><?php echo icon('signin'); ?> Iniciar Sesión</button>
-
-				<?php echo form_close(); ?>
-
-			<?php endif; ?>
+			<p class="navbar-text pull-right">Sesión iniciada como <strong><?php echo $this->session->userdata('name'); ?></strong> &mdash; <?php echo anchor('logout', 'salir'); ?></p>
 
 		</div>
 
---
+++ application/views/layouts/site.php
@@ -0,0 +1,51 @@
+<!-- header -->
+<?php  $this->load->view('assets/inc/header'); ?>
+
+<!-- navbar -->
+<div class="navbar navbar-static-top">
+
+	<div class="navbar-inner">
+
+		<div class="container">
+
+			<?php echo anchor('#', 'SAV', array('class' => 'brand')); ?>
+
+			<ul class="nav">
+
+				<li><?php echo anchor('#', 'Guía de Usuario'); ?></li>
+				<li><?php echo anchor('#', 'Preguntas Frecuentes'); ?></li>
+				<li><?php echo anchor('#', 'Soporte'); ?></li>
+
+			</ul>
+
+			<?php if ($this->init->hasSession()): ?>
+
+				<p class="navbar-text pull-right">Sesión iniciada como <strong><?php echo $this->session->userdata('name'); ?></strong> &mdash; <?php echo anchor('dashboard', 'ir a area de cliente'); ?> | <?php echo anchor('logout', 'salir'); ?></p>
+
+			<?php else: ?>
+
+				<?php echo form_open('login', array('class' => 'navbar-form pull-right')); ?>
+
+					<input type="text" class="span2" name="username" placeholder="Usuario" />
+					<input type="password" class="span2" name="password" placeholder="Contraseña" />
+					<button class="btn" id="login-submit"><?php echo icon('signin'); ?> Iniciar Sesión</button>
+
+				<?php echo form_close(); ?>
+
+			<?php endif; ?>
+
+		</div>
+
+	</div>
+
+</div>
+
+<!-- content -->
+<div class="container">
+
+	<?php echo $yield; ?>
+
+</div>
+
+<!-- footer -->
+<?php $this->load->view('assets/inc/footer'); ?>

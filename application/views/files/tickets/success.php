a7815215fbe344dabaf3ebe1cbbb895892c6ca02
Mario Cuba <mario@mariocuba.net>
2012-08-24 23:39:11 -0430

    Updated the success view for a more wide view and better links.

--- application/views/files/tickets/success.php
+++ application/views/files/tickets/success.php
@@ -1,15 +1,27 @@
-<div class="span4 offset4">
+<div class="span6 offset3">
 
 	<div class="page-header">
 
-		<h4>Consulta enviada</h4>
+		<h4>¡Muchas gracias!</h4>
 
 	</div>
 
-	<p>Su número de consulta es <strong><?php echo $ticket; ?></strong> &mdash; este es su número identificador en caso de que quiera buscarla más adelante.</p>
+	<p>Su número de consulta es <strong>#<?php echo $ticket; ?></strong> &mdash; este es su número identificador en caso de que quiera buscarla más adelante en el <?php echo anchor('#', 'listado'); ?>.</p>
 
-	<p>Nosotros nos encargaremos de responderle por esta misma vía cuando lo hayamos atendido. ¡Muchas gracias!</p>
+	<p>Nosotros nos encargaremos de responderle por esta misma vía cuando lo hayamos atendido.</p>
 
-	<p><?php echo anchor('dashboard', 'Regresar', array('class' => 'btn btn-green btn-block')); ?></p>
+	<p><strong>¡Muchas gracias!</strong></p>
+
+	<br />
+
+	<!-- options -->
+	<div class="btn-group pagination-centered">
+
+		<?php echo anchor('tickets/add', icon('plus-sign') . ' Otra Consulta', array('class' => 'btn')); ?>
+		<?php echo anchor('#', icon('list-ul') . ' Ir al Listado', array('class' => 'btn')); ?>
+		<?php echo anchor('dashboard', icon('home') . ' Ir al Inicio', array('class' => 'btn')); ?>
+
+	</div>
+	<!-- end options -->
 
 </div>

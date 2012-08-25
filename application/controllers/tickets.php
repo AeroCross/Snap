cabfce6e2d2ca9db341ce0bc81cb75e6a82742c7
Mario Cuba <mario@mariocuba.net>
2012-08-24 23:12:49 -0430

    Added the _process() and success() methods. Fixed comments.

--- application/controllers/tickets.php
+++ application/controllers/tickets.php
@@ -18,6 +18,9 @@ class Tickets extends SAV_Controller {
 	*/
 	public function __construct() {
 		parent::__construct();
+
+		// load the notification presenter
+		$this->load->presenter('notification');
 	}
 
 	public function index() {
@@ -25,13 +28,66 @@ class Tickets extends SAV_Controller {
 	}
 	
  	/**
- 	* Description.
+ 	* Displays the add ticket form.
  	*
  	* @access	public
  	*/
 	public function add() {
+
+		// if a ticket was tried to be registered
+		if ($this->input->post() != FALSE) {
+			$this->presenter->notification->create($this->_process());
+		}
+
 		$this->load->presenter('form');
 		$this->view = 'files/tickets/add';
+	}
+
+ 	/**
+ 	* Displays a feedback message post-ticket.
+ 	*
+ 	* @access	public
+ 	*/
+	public function success() {
+		// check the flashdata
+		$id = $this->session->flashdata('ticket');
+
+		// proceed with feedback
+		if (!empty($id)) {
+			$this->data->ticket = $this->session->flashdata('ticket');
+		} else {
+			redirect('tickets/add');
+		}
+	}
+
+	/**
+ 	* Processes a new ticket.
+ 	*
+ 	* @access	public
+ 	*/
+	public function _process() {
+
+		$data = array(
+			'department'	=> $this->input->post('department'),
+			'subject'		=> $this->input->post('subject'),
+			'content'		=> $this->input->post('content'),
+		);
+		
+		$this->load->model('sav_ticket');
+		$id = $this->sav_ticket->addTicket($data);
+
+		if (!empty($id)) {
+			$this->session->set_flashdata('ticket', $id);
+			redirect('tickets/success');
+
+		// for some reason, we couldn''t insert the data
+		} else {
+			return array(
+				'status'	=> 'failed',
+				'message'	=> 'Error al intentar ingresar la consulta.',
+				'type'		=> 'error'
+			);
+		}
 	}
 }
 

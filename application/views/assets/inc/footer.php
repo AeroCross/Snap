52dd265746e064865e3ac267ad7d73cfd6b96c11
Mario Cuba <mario@mariocuba.net>
2012-09-03 21:10:33 -0430

    Whitespace changes.

--- application/views/assets/inc/footer.php
+++ application/views/assets/inc/footer.php
@@ -1,24 +1,24 @@
-<div class="container-fluid">
+		<div class="container-fluid">
 
-	<footer class="row-fluid footer">
+			<footer class="row-fluid footer">
 
-		<div class="span6">
+				<div class="span6">
 
-			<strong>SAAV</strong> versión <strong>0.1</strong> &mdash; Sistema Automatizado de Asistencia Virtual<br />
-			Coyright 2012 &copy; <?php echo anchor('http://ingenium-dv.com', 'Ingenium: Desarrollo Virtual'); ?> &mdash; Todos los derechos reservados
+					<strong>SAAV</strong> versión <strong>0.1</strong> &mdash; Sistema Automatizado de Asistencia Virtual<br />
+					Coyright 2012 &copy; <?php echo anchor('http://ingenium-dv.com', 'Ingenium: Desarrollo Virtual'); ?> &mdash; Todos los derechos reservados
+
+				</div>
+
+				<div class="span6">
+
+					<img src="<?php echo $this->resource->img('idv-logo.png'); ?>" alt="" width="48" height="48" class="logo" />
+
+				</div>
+
+			</footer>
 
 		</div>
 
-		<div class="span6">
-
-			<img src="<?php echo $this->resource->img('idv-logo.png'); ?>" alt="" width="48" height="48" class="logo" />
-
-		</div>
-
-	</footer>
-
-</div>
-
-</body>
+	</body>
 
 </html>

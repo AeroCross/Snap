19ff35bfdb80df41c43c8161677b6e58dc5a2f38
Mario Cuba <mario@mariocuba.net>
2012-09-06 16:18:47 -0430

    Show / hide correct input.

--- application/views/assets/js/app.js
+++ application/views/assets/js/app.js
@@ -18,17 +18,30 @@ jQuery(document).ready(function() {
 		// fields
 		fieldDepartment = jQuery('#department');
 		fieldStatus		= jQuery('#status');
+		fieldCompany	= jQuery('#company');
+		fieldReported	= jQuery('#reported_by');
 		fieldValue		= jQuery('#value');
 
+		// @TODO: this could be replaced by a option container, instad of the whole input
 		switch(value) {
 			case 'id':
 			case 'subject':
-				fieldDepartment
+				fieldReported
 					.hide()
 					.val('')
 					.attr('disabled', 'disabled');
 
 				fieldStatus
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldDepartment
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldCompany
 					.hide()
 					.val('')
 					.attr('disabled', 'disabled');
@@ -49,6 +62,16 @@ jQuery(document).ready(function() {
 					.val('')
 					.attr('disabled', 'disabled');
 
+				fieldReported
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldCompany
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
 				fieldDepartment
 					.show()
 					.removeAttr('disabled');
@@ -60,12 +83,74 @@ jQuery(document).ready(function() {
 					.val('')
 					.attr('disabled', 'disabled');
 
+				fieldReported
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
 				fieldDepartment
 					.hide()
 					.val('')
 					.attr('disabled', 'disabled');
 
+				fieldCompany
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
 				fieldStatus
+					.show()
+					.removeAttr('disabled');
+			break;
+
+			case 'reported_by':
+				fieldValue
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldStatus
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldDepartment
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldCompany
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldReported
+					.show()
+					.removeAttr('disabled');
+			break;
+
+			case 'company':
+				fieldValue
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldStatus
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldDepartment
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldReported
+					.hide()
+					.val('')
+					.attr('disabled', 'disabled');
+
+				fieldCompany
 					.show()
 					.removeAttr('disabled');
 			break;

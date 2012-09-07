161fc0c330ae4e133caa0cbe2d94f8c0978d88b1
Mario Cuba <mario@mariocuba.net>
2012-09-07 16:42:07 -0430

    Rename resources with shorter versions.

--- application/views/assets/inc/header.php
+++ application/views/assets/inc/header.php
@@ -10,14 +10,15 @@
 		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700|Arvo' rel='stylesheet' type='text/css'>
 		
 		<!-- styles -->
-		<link rel="stylesheet" href="<?php echo $this->resource->css('bootstrap.css'); ?>" />
-		<link rel="stylesheet" href="<?php echo $this->resource->css('bootstrap-responsive.css'); ?>" />
-		<link rel="stylesheet" href="<?php echo $this->resource->css('font-awesome.css'); ?>" />
+		<link rel="stylesheet" href="<?php echo $this->resource->css('bs.css'); ?>" />
+		<link rel="stylesheet" href="<?php echo $this->resource->css('bs-r.css'); ?>" />
+		<link rel="stylesheet" href="<?php echo $this->resource->css('fa.css'); ?>" />
+		<link rel="stylesheet" href="<?php echo $this->resource->css('fa-ie.css'); ?>" />
 		<link rel="stylesheet" href="<?php echo $this->resource->css('app.css'); ?>" />
 
 		<!-- scripts -->
-		<script src="<?php echo $this->resource->js('jquery.js'); ?>"></script>
-		<script src="<?php echo $this->resource->js('bootstrap.js'); ?>"></script>
+		<script src="<?php echo $this->resource->js('jq.js'); ?>"></script>
+		<script src="<?php echo $this->resource->js('bs.js'); ?>"></script>
 		<script src="<?php echo $this->resource->js('app.js'); ?>"></script>
 
 	</head>

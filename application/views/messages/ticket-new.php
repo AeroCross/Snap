e0bae523a77230fc1129f74db7eae274bdceea9c
Mario Cuba <mario@mariocuba.net>
2012-09-08 21:55:34 -0430

    Pre-formatted email.

--- application/views/messages/ticket-new.php
+++ application/views/messages/ticket-new.php
@@ -1,7 +1,13 @@
 <p>Una nueva consulta ha sido agregada al sistema por: <strong><?php echo $reported_by; ?></strong></p>
 
 <blockquote>
-	<?php echo $content; ?>
+
+	<pre>
+	
+		<?php echo $content; ?>
+	
+	</pre>
+
 </blockquote>
 
 <p>Para ingresar a la consulta: <?php echo anchor('tickets/view/' . $ticket_id); ?></p>

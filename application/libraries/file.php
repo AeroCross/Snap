9541c492bd170eea14f553588b4b1052835b9b9a
Mario Cuba <mario@mariocuba.net>
2012-09-16 22:30:03 -0430

    Add jpeg to allowed files.

--- application/libraries/file.php
+++ application/libraries/file.php
@@ -47,7 +47,7 @@ class File {
 		$this->app->load->library('upload');
 		$config = array(
 			'upload_path'	=> './files/tickets/' . $id . '/' . $this->app->session->userdata('id') . '/',
-			'allowed_types'	=> 'bmp|gif|jpg|png|psd|doc|docx|txt|zip|tar.gz|tar|tar.bz|rar|ppt|pptx|xls|xlsx|csv|ai',
+			'allowed_types'	=> 'bmp|gif|jpg|jpeg|png|psd|doc|docx|txt|zip|tar.gz|tar|tar.bz|rar|ppt|pptx|xls|xlsx|csv|ai',
 			'remove_spaces'	=> FALSE
 		);
 

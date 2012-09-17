4c572759da79733541d578b94328274a7f0a01a1
Mario Cuba <mario@mariocuba.net>
2012-09-16 22:44:39 -0430

    Remove allowed types.

--- application/libraries/file.php
+++ application/libraries/file.php
@@ -47,7 +47,6 @@ class File {
 		$this->app->load->library('upload');
 		$config = array(
 			'upload_path'	=> './files/tickets/' . $id . '/' . $this->app->session->userdata('id') . '/',
-			'allowed_types'	=> 'bmp|gif|jpg|jpeg|png|psd|doc|docx|txt|zip|tar.gz|tar|tar.bz|rar|ppt|pptx|xls|xlsx|csv|ai',
 			'remove_spaces'	=> FALSE
 		);
 

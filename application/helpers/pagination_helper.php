311f7925fd8597bd1d738604d31d373fbb473f2a
Mario Cuba <mario@mariocuba.net>
2012-09-20 20:48:57 -0430

    Add pagination helper and configuration file.

---
+++ application/helpers/pagination_helper.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+/**
+* Calculates limit and offset for pagination.
+*
+* @param	int		- the uri segment of the page
+* @return	object	- the limit and the offset
+*/
+function calculateOffset($uri_segment) {
+	// get the global object
+	$app =& get_instance();
+
+	$pagination			= new StdClass;
+	$pagination->limit	= 8; // should be a database value
+	$value				= $app->uri->segment($uri_segment);
+	
+	if (empty($value)) {
+		$uri = 1;
+	} else {
+		$uri = 	(int) $app->uri->segment($uri_segment);
+	}
+
+	$pagination->offset	= ($uri * $pagination->limit) - $pagination->limit;
+
+	return $pagination;
+}
+	
+/* End of file pagination_helper.php */
+/* Location: ./application/helpers/pagination_helper.php */

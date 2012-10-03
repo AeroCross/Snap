d48efa3812a1d3b11b6d7bce459042f4a42448ea
Mario Cuba <mario@mariocuba.net>
2012-10-03 12:16:37 -0430

    Default avatar. Set it when no avatar was found.
    Also added correct comments for the class.

--- application/presenters/user.php
+++ application/presenters/user.php
@@ -1,6 +1,34 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+* User presenter.
+*
+* Handles the display of user-related objects.
+*
+* @package		SAAV
+* @subpackage	Presenters
+* @author		Mario Cuba <mario@mariocuba.net>
+*/
 class UserPresenter {
+
+	/**
+	* The class constructor.
+	*
+	* @access	public
+	*/
+	public function __construct() {
+		$this->app = & get_instance();
+	}
+
+	/**
+	* Shows the user avatar.
+	*
+	* @param	int		- the user id for the avatar
+	* @param	int		- the width of the image
+	* @param	int		- the height of the image
+	* @return	string	- the html img tag with the avatar
+	* @access	public
+	*/
 	public function avatar($user_id, $width = 200, $height = null) {
 		$file = FCPATH . 'files/avatars/' . $user_id . '/avatar.jpg';
 
@@ -9,9 +37,9 @@ class UserPresenter {
 		}
 
 		if (!file_exists($file)) {
-			return '<img src="http://placehold.it/' . $width . 'x' . $height . '" alt="" width="' . $width . '" height ="' . $height . '" />';
+			return '<img src="' . $this->app->resource->img('avatars/default.png') . '" alt="" width="' . $width . '" height ="' . $height . '" />';
 		}else {
-			return '<img src="' . base_url('files/avatars/' . $user_id . '/avatar.jpg') . '" alt="" width="' . $width . '" height="' . $height . '" />';
+			return '<img src="' . base_url('files/avatars/' . $user_id . '/avatar.jpg?' . time()) . '" alt="" width="' . $width . '" height="' . $height . '" />';
 		}
 	}
 }

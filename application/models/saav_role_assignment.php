835d7760b9956a3325cc95d633e578d9430e1b3c
Mario Cuba <mario@mariocuba.net>
2012-10-09 09:28:05 -0430

    Add getRoleUsers method.

--- application/models/saav_role_assignment.php
+++ application/models/saav_role_assignment.php
@@ -22,6 +22,23 @@ class Saav_role_assignment extends EXT_Model {
 	public function __construct() {
 		parent::__construct();
 	}
+
+	public function getRoleUsers($role = null) {
+		// if empty, get them all
+		if (empty($role)) {
+			return $this->db->select()->get($this->_table)->result();
+		}
+
+		// transform string to int
+		switch ($role) {
+			case 'client':	$role = 3; break;
+			case 'support':	$role = 2; break;
+			case 'admin':	$role = 1; break;
+			default: $this->getRoleUsers(); break;
+		}
+
+		return $this->db->select()->where('role_id', $role)->get($this->_table)->result();
+	}
 }
 
 /* End of file saav_role_assignments.php */

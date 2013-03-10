5bd29d66cbee8acf8d9ddce47c977934d3387988
Mario Cuba <mario@mariocuba.net>
2013-03-09 21:03:02 -0430

    Comments on the Helper library.

--- application/libraries/helper.php
+++ application/libraries/helper.php
@@ -1,7 +1,21 @@
 <?php
 
+/**
+* Various helper methods
+*
+* @package		SAAV
+* @subpackage	Libraries
+* @author		Mario Cuba <mario@mariocuba.net>
+*/
 class Helper {
 
+	/**
+	* Parses the value of a ticket status
+	*
+	* @param	string	- the ticket status
+	* @return	string	- an icon referencing the status
+	* @access	public
+	*/
     public static function status($status) {
         switch ($status) {
         	case 'closed':	$status = 'ok-sign text-success'; break;
@@ -12,6 +26,13 @@ class Helper {
         return '<i class="icon-' . $status . '"></i> ';
     }
 
+    /**
+    * Creates the markup for an icon
+    *
+    * @param	string	- the name of the icon
+    * @return	string	- the markup of the icon
+    * @access	public
+    */
     public static function icon($icon) {
     	return '<i class="icon-' . $icon . '"></i>';
     }

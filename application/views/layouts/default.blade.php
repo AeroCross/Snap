a575d462be1693321f3a31b22d0c315ef8af8553
Mario Cuba <mario@mariocuba.net>
2013-03-11 21:56:56 -0430

    Sticky footer.

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -16,93 +16,118 @@
 	</head>
 
 	<body>
+		
+		<div id="wrap">
 
-		<!-- navbar -->
-		<div class="navbar navbar-fixed-top navbar-googlebar">
+			<!-- navbar -->
+			<div class="navbar navbar-fixed-top navbar-googlebar">
 
-			<div class="navbar-inner">
+				<div class="navbar-inner">
 
-				<div class="container">
+					<div class="container">
 
-					<a class="brand" href="#">{{ HTML::image('img/logo.png', 'SAAV Logo', array('width' => '24', 'height' => '24')) }}</a>
+						<a class="brand" href="#">{{ HTML::image('img/logo.png', 'SAAV Logo', array('width' => '24', 'height' => '24')) }}</a>
 
-					<ul class="nav navbar-googlebar">
+						<ul class="nav navbar-googlebar">
 
-						<li class="dropdown">
+							<li class="dropdown">
 
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Consultas <b class="caret"></b></a>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Consultas <b class="caret"></b></a>
 
-							<ul class="dropdown-menu">
+								<ul class="dropdown-menu">
 
-								<li><a href="#">Nueva consulta</a></li>
-								<li><a href="#">Mis consultas</a></li>
+									<li><a href="#">Nueva consulta</a></li>
+									<li><a href="#">Mis consultas</a></li>
 
-							</ul>
+								</ul>
 
-						</li>
+							</li>
 
-						<li class="dropdown">
+							<li class="dropdown">
 
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Administración <b class="caret"></b></a>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Administración <b class="caret"></b></a>
 
-							<ul class="dropdown-menu">
+								<ul class="dropdown-menu">
 
-								<li class="nav-header">Consultas</li>
+									<li class="nav-header">Consultas</li>
 
-								<li><a href="#">Todas las Consultas</a></li>
+									<li><a href="#">Todas las Consultas</a></li>
 
-									<li class="divider"></li>
-									<li class="nav-header">Usuarios</li>
+										<li class="divider"></li>
+										<li class="nav-header">Usuarios</li>
 
-									<li><a href="#">Agregar Usuario</a></li>
-									<li><a href="#">Asignar Roles</a></li>
-									<li><a href="#">Compañías</a></li>
+										<li><a href="#">Agregar Usuario</a></li>
+										<li><a href="#">Asignar Roles</a></li>
+										<li><a href="#">Compañías</a></li>
 
-									<li class="divider"></li>
-									<li class="nav-header">Sistema</li>
+										<li class="divider"></li>
+										<li class="nav-header">Sistema</li>
 
-									<li><a href="#">Configuración</a></li>
-									<li><a href="#">Actualizar</a></li>
-								
-							</ul>
+										<li><a href="#">Configuración</a></li>
+										<li><a href="#">Actualizar</a></li>
+									
+								</ul>
 
-						</li>
+							</li>
 
-						<li class="divider-vertical"></li>
+							<li class="divider-vertical"></li>
 
-						<li><a href="#"><i class="icon-wrench"></i> Soporte</a></li>
+							<li><a href="#"><i class="icon-wrench"></i> Soporte</a></li>
 
-					</ul>
+						</ul>
 
-					<ul class="nav navbar-googlebar pull-right">
+						<ul class="nav navbar-googlebar pull-right">
 
-						<li class="dropdown">
+							<li class="dropdown">
 
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i> Sesión iniciada como <strong>{{ Session::get('name') }}</strong> <b class="caret"></b></a>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i> Sesión iniciada como <strong>{{ Session::get('name') }}</strong> <b class="caret"></b></a>
 
-							<ul class="dropdown-menu">
+								<ul class="dropdown-menu">
 
-								<li><a href="#"><i class="icon-edit"></i> Editar Perfil</a></li>
-								<li><a href="{{ URL::to('logout') }}"><i class="icon-signout"></i> Cerrar Sesión</a></li>
+									<li><a href="#"><i class="icon-edit"></i> Editar Perfil</a></li>
+									<li><a href="{{ URL::to('logout') }}"><i class="icon-signout"></i> Cerrar Sesión</a></li>
 
-							</ul>
+								</ul>
 
-						</li>
+							</li>
 
-					</ul>
+						</ul>
+
+					</div>
 
 				</div>
 
 			</div>
+			<!-- navbar end -->
+
+			<!-- container -->
+			<div class="container" id="main-container">
+					
+					@yield('content')
+
+			</div>
+			<!-- container end -->
+
+			<div id="push"></div>
+		
+		</div>
+
+		<!-- footer -->
+		<div id="footer">
+
+			<div class="container">
+
+				<p class="muted credit">
+
+					<strong>SAAV</strong> Versión <strong>0.5a</strong> — Sistem Automatizado de Asistencia Virtual<br />
+					Copyright 2013 &copy; {{ HTML::link('http://mariocuba.net', 'Mario Cuba') }} — Todos los derechos reservados.
+
+				</p>
+
+			</div>
 
 		</div>
-		<!-- end navbar -->
-		
-		<div class="container">
-
-			@yield('content')
-
-		</div> <!-- /container -->
+		<!-- footer end -->
 
 		{{ Asset::scripts(); }}
 

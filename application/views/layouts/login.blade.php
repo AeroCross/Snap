a5031c72763d0d46097e1c4814477eabf4a32eb8
Mario Cuba <mario@mariocuba.net>
2013-04-21 19:10:59 -0430

    Cooler title breadcrumb separator.

--- application/views/layouts/login.blade.php
+++ application/views/layouts/login.blade.php
@@ -5,7 +5,7 @@
 
 		<meta charset="utf-8">
 
-		<title>Snap | {{ $title }}</title>
+		<title>Snap » {{ $title }}</title>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="description" content="" />

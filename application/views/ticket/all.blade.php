156503f46fe8f5c31d959d6d80b7dd63ba934788
Mario Cuba <mario@mariocuba.net>
2013-04-21 18:39:00 -0430

    Better all tickets view.

--- application/views/ticket/all.blade.php
+++ application/views/ticket/all.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row padded">
 	
-	<table class="table table-striped table-bordered">
+	<table class="table table-striped table-hover table-bordered table-tickets">
 
 		<thead>
 
@@ -14,8 +14,6 @@
 				<th>Asunto</th>
 				<th>Reportado por</th>
 				<th>Asignado a</th>
-				<th>Creado</th>
-				<th>Modificado</th>
 				<th>Estatus</th>
 
 			</tr>
@@ -30,6 +28,7 @@
 					foreach($users as $user) {
 						if ($user->id == $ticket->reported_by) {
 							$reported = $user;
+							$reported->name = $reported->firstname . ' ' . $reported->lastname;
 						}
 					}
 
@@ -40,8 +39,12 @@
 						foreach($users as $user) {
 							if ($user->id == $ticket->assigned_to) {
 								$assigned = $user;
+								$assigned->name = $assigned->firstname . ' ' . $assigned->lastname;
 							}
 						}
+					} else {
+							$assigned = new StdClass;
+							$assigned->name = '<span class="muted">Nadie</span>';
 					}
 
 					// for consistency
@@ -51,18 +54,9 @@
 				<tr>
 
 					<td>{{ $ticket->id }}</td>
-					<td>{{ HTML::link('ticket/' . $ticket->id, $ticket->subject) }}</td>
-					<td>{{ $reported->firstname . ' ' . $reported->lastname }}</td>
-
-					{{-- show nothing if noone's assigned --}}
-					@if (isset($assigned))
-						<td>{{ $assigned->firstname . ' ' . $assigned->lastname }}</td>
-					@else
-						<td><span class="muted">Nadie</span></td>
-					@endif
-
-					<td>{{ $ticket->created_at }}</td>
-					<td>{{ $ticket->updated_at }}</td>
+					<td><p>{{ HTML::link('ticket/' . $ticket->id, $ticket->subject) }}</p><small><strong>Creado:</strong>{{ $ticket->created_at }}</small><br /><small><strong>Última actualización:</strong> {{ $ticket->updated_at }}</small></td>
+					<td>{{ $reported->name }}</td>
+					<td>{{ $assigned->name }}</td>
 					<td>{{ Helper::status($ticket->status) }}</td>
 
 				</tr>
